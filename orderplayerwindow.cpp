9e8a00205531dcf5536c68784bd8c4a37012706a
MRXY001 <wxy19980615@gmail.com>
2020-11-23 15:45:03 +0800

    显示搜索结果；优化界面

--- orderplayerwindow.cpp
+++ orderplayerwindow.cpp
@@ -2,10 +2,19 @@
 #include "ui_orderplayerwindow.h"
 
 OrderPlayerWindow::OrderPlayerWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::OrderPlayerWindow)
+    : QMainWindow(parent),
+      ui(new Ui::OrderPlayerWindow),
+      settings("settings.ini", QSettings::Format::IniFormat)
 {
     ui->setupUi(this);
+
+    QHeaderView* header = ui->searchResultTable->horizontalHeader();
+    header->setSectionResizeMode(QHeaderView::ResizeToContents);
+    header->setMinimumSectionSize(QFontMetrics(this->font()).horizontalAdvance("哈哈哈哈哈哈"));
+    header->setDefaultAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    header->setStyleSheet("QHeaderView { background-color: transparent; }");
+    ui->searchResultTable->verticalHeader()->setStyleSheet("QHeaderView { background-color: transparent; }");
+    ui->searchResultTable->setItemDelegate(new NoFocusDelegate());
 }
 
 OrderPlayerWindow::~OrderPlayerWindow()
@@ -13,3 +22,104 @@ OrderPlayerWindow::~OrderPlayerWindow()
     delete ui;
 }
 
+
+void OrderPlayerWindow::on_searchEdit_returnPressed()
+{
+    QString text = ui->searchEdit->text();
+    searchMusic(text);
+}
+
+void OrderPlayerWindow::on_searchButton_clicked()
+{
+    on_searchEdit_returnPressed();
+}
+
+/**
+ * 搜索音乐
+ */
+void OrderPlayerWindow::searchMusic(QString key)
+{
+    if (key.trimmed().isEmpty())
+        return ;
+    QString url = "http://iwxyi.com:3000/search?keywords=" + key.toUtf8().toPercentEncoding();
+    QNetworkAccessManager* manager = new QNetworkAccessManager;
+    QNetworkRequest* request = new QNetworkRequest(url);
+    request->setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded; charset=UTF-8");
+    request->setHeader(QNetworkRequest::UserAgentHeader, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+    connect(manager, &QNetworkAccessManager::finished, this, [=](QNetworkReply* reply){
+        QByteArray data = reply->readAll();
+        QJsonParseError error;
+        QJsonDocument document = QJsonDocument::fromJson(data, &error);
+        if (error.error != QJsonParseError::NoError)
+        {
+            qDebug() << error.errorString();
+            return ;
+        }
+        QJsonObject json = document.object();
+        if (json.value("code").toInt() != 200)
+        {
+            qDebug() << ("返回结果不为200：") << json.value("message").toString();
+            return ;
+        }
+
+        QJsonArray songs = json.value("result").toObject().value("songs").toArray();
+        searchResultSongs.clear();
+        foreach (QJsonValue val, songs)
+        {
+            searchResultSongs << Song::fromJson(val.toObject());
+        }
+
+        setSearchResultTable(searchResultSongs);
+    });
+    manager->get(*request);
+}
+
+void OrderPlayerWindow::setSearchResultTable(QList<Song> songs)
+{
+    QTableWidget* table = ui->searchResultTable;
+    enum {
+        titleCol,
+        artistCol,
+        albumCol,
+        durationCol,
+        markCol
+    };
+
+    QFontMetrics fm(font());
+    int fw = fm.horizontalAdvance("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
+    auto createItem = [=](QString s){
+        QTableWidgetItem *item = new QTableWidgetItem();
+        if (s.length() > 16 && fm.horizontalAdvance(s) > fw)
+        {
+            item->setToolTip(s);
+            s = s.left(15) + "...";
+        }
+        item->setText(s);
+        return item;
+    };
+
+    table->setRowCount(songs.size());
+    for (int row = 0; row < songs.size(); row++)
+    {
+        Song song = songs.at(row);
+        table->setItem(row, titleCol, createItem(song.name));
+        table->setItem(row, artistCol, createItem(song.artistNames));
+        table->setItem(row, albumCol, createItem(song.album.name));
+        QString dur = QString("%1:%2").arg(song.duration/1000 / 60, 2, 10, QLatin1Char('0'))
+                .arg(song.duration/1000 % 60, 2, 10, QLatin1Char('0'));
+        table->setItem(row, durationCol, createItem(dur));
+        table->setItem(row, markCol, createItem(snum(song.mark)));
+    }
+}
+
+void OrderPlayerWindow::showEvent(QShowEvent *)
+{
+    restoreGeometry(settings.value("orderplayerwindow/geometry").toByteArray());
+    restoreState(settings.value("orderplayerwindow/state").toByteArray());
+}
+
+void OrderPlayerWindow::closeEvent(QCloseEvent *)
+{
+    settings.setValue("orderplayerwindow/geometry", this->saveGeometry());
+    settings.setValue("orderplayerwindow/state", this->saveState());
+}
--- orderplayerwindow.h
+++ orderplayerwindow.h
@@ -2,10 +2,100 @@
 #define ORDERPLAYERWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QSettings>
+#include <QJsonDocument>
+#include <QJsonParseError>
+#include <QDebug>
+#include <QApplication>
+#include <QClipboard>
+#include <QNetworkCookie>
+#include <QMessageBox>
+#include <QInputDialog>
+#include <QTextCodec>
+#include <stdio.h>
+#include <iostream>
+#include <QtWebSockets/QWebSocket>
+#include <QAuthenticator>
+#include <QtConcurrent/QtConcurrent>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QStyledItemDelegate>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class OrderPlayerWindow; }
 QT_END_NAMESPACE
+
+#define JSON_VAL_LONG(json, x) static_cast<qint64>(json.value(#x).toDouble())
+#define JVAL_LONG(x) static_cast<qint64>(json.value(#x).toDouble())
+#define JVAL_INT(x) json.value(#x).toInt()
+#define JVAL_STR(x) json.value(#x).toString()
+#define snum(x) QString::number(x)
+
+struct Artist
+{
+    qint64 id;
+    QString name;
+    QString faceUrl;
+
+    static Artist fromJson(QJsonObject json)
+    {
+        Artist artist;
+        artist.id = JVAL_LONG(id);
+        artist.name = JVAL_STR(name);
+        artist.faceUrl = JVAL_STR(img1v1Url);
+        return artist;
+    }
+};
+
+struct Album
+{
+    qint64 id;
+    QString name;
+    int size;
+    int mark;
+
+    static Album fromJson(QJsonObject json)
+    {
+        Album album;
+        album.id = JVAL_LONG(id);
+        album.name = JVAL_STR(name);
+        album.size = JVAL_INT(size);
+        album.mark = JVAL_INT(mark);
+        return album;
+    }
+};
+
+struct Song
+{
+    qint64 id;
+    QString name;
+    int duration;
+    int mark;
+    QList<Artist> artists;
+    Album album;
+    QString artistNames;
+
+    static Song fromJson(QJsonObject json)
+    {
+        Song song;
+        song.id = JVAL_LONG(id);
+        song.name = JVAL_STR(name);
+        QJsonArray array = json.value("artists").toArray();
+        QStringList artistNameList;
+        foreach (QJsonValue val, array)
+        {
+            Artist artist = Artist::fromJson(val.toObject());
+            song.artists.append(artist);
+            artistNameList.append(artist.name);
+        }
+        song.artistNames = artistNameList.join("/");
+        song.album = Album::fromJson(json.value("album").toObject());
+        song.duration = JVAL_INT(duration);
+        song.mark = JVAL_INT(mark);
+        return song;
+    }
+};
 
 class OrderPlayerWindow : public QMainWindow
 {
@@ -15,7 +105,42 @@ public:
     OrderPlayerWindow(QWidget *parent = nullptr);
     ~OrderPlayerWindow();
 
+private slots:
+    void on_searchEdit_returnPressed();
+
+    void on_searchButton_clicked();
+
+private:
+    void searchMusic(QString key);
+    void setSearchResultTable(QList<Song> songs);
+
+protected:
+    void showEvent(QShowEvent*) override;
+    void closeEvent(QCloseEvent*) override;
+
 private:
     Ui::OrderPlayerWindow *ui;
+    QSettings settings;
+    QList<Song> searchResultSongs;
 };
+
+class NoFocusDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    NoFocusDelegate(){}
+    ~NoFocusDelegate(){}
+
+    void paint(QPainter* painter, const QStyleOptionViewItem & option, const QModelIndex &index) const
+    {
+        QStyleOptionViewItem itemOption(option);
+        if (itemOption.state & QStyle::State_HasFocus)
+        {
+            itemOption.state = itemOption.state ^ QStyle::State_HasFocus;
+        }
+        QStyledItemDelegate::paint(painter, itemOption, index);
+    }
+};
+
 #endif // ORDERPLAYERWINDOW_H
